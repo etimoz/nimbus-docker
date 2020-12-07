0f89554cadda5e3eebeb48512d50a97154bacaab
etimoz <etimoz@users.noreply.github.com>
2020-12-07 17:35:58 +0100

    changed to default port 9000

--- scripts/start-beacon-node-and-validator.sh
+++ scripts/start-beacon-node-and-validator.sh
@@ -4,4 +4,4 @@ if [ "$IMPORT_LAUNCHPAD_KEYSTORES" != "" ]; then
   echo $KEYSTORE_PWD | /usr/src/nimbus-eth2/build/nimbus_beacon_node --data-dir=/var/lib/nimbus --log-file=/var/lib/nimbus/beacon_node.log --network=$NETWORK deposits import /var/lib/nimbus/validator_keys
 fi
 
-/usr/src/nimbus-eth2/build/nimbus_beacon_node --tcp-port=9100 --udp-port=9100 --web3-url=$VOTING_ETH1_NODE --data-dir=/var/lib/nimbus --log-file=/var/lib/nimbus/beacon_node.log --network=$NETWORK --graffiti="$GRAFFITI"
+/usr/src/nimbus-eth2/build/nimbus_beacon_node --tcp-port=9000 --udp-port=9000 --web3-url=$VOTING_ETH1_NODE --data-dir=/var/lib/nimbus --log-file=/var/lib/nimbus/beacon_node.log --network=$NETWORK --graffiti="$GRAFFITI"
